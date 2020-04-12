ebb2d981703217e63087d960223e0c24a08dd24c
Alexandros Kapeletzis <alex.kapele@gmail.com>
2020-04-12 20:16:59 +0100

    Add files via upload

--- gym-market/gym_market/envs/market_env.py
+++ gym-market/gym_market/envs/market_env.py
@@ -44,24 +44,24 @@ class MarketEnv(gym.Env):
     
     def step(self, action):
         assert self.action_space.contains(action), "%r (%s) invalid"%(action, type(action))
+        
+        #Get global variables
         trade_log = self.trade_log
         pos_limit = self.pos_limit
-        
         action_log = self.action_log
         pos_log = self.pos_log
-               
+        
+        #Get state and action
         state = self.state
-        action = self.action_encoding[action]
-        
         features_t,pos,pos_diff,time_to_complete,stop_loss,take_profit = state
+        action = self.action_encoding[action] #convert action from 0,1,2 to 0,1,-1
         
-
-        
+        #Check stop_loss and take_profit
         self.stop_loss = True if self.active_return < self.stop_loss_thres else False
-        self.stop_loss_log.append(self.stop_loss)
+        self.stop_loss_log.append(self.stop_loss) #Save stop_loss log for each step
         self.take_profit = True if self.active_return > self.take_prof_thres else False
         
-
+        #Take action (depending on pos_limit & stop_loss/take_profit
         if action*pos>=0 and self.stop_loss==False and self.take_profit==False:
             if abs(pos) < pos_limit:
                 trade_log.append(action)
@@ -75,58 +75,59 @@ class MarketEnv(gym.Env):
             pos = 0 #action #close all existing positions and apply current action          
             self.hold_time = abs(pos) #reset hold time
         
+        #Calculate inactive time for pernalty (i.e. no position)
         if action == 0 and pos == 0:
             self.inactive_time += 1
         else:
             self.inactive_time = 0         
         
+        #Store day when last position was openned (for stop_loss & take_profit calc)
+        if (pos_log[self.window_counter]!=0) and (pos_log[self.window_counter-1]==0):        
+            self.day_pos = self.window_counter
+        
+        #Save logs
         action_log.append(action)
         pos_log.append(pos)  
         self.action_log = action_log
         self.pos_log = pos_log
-        
-        if (pos_log[self.window_counter]!=0) and (pos_log[self.window_counter-1]==0):        
-            self.day_pos = self.window_counter
-        
         self.trade_log = trade_log
-        
+               
+        #Check if episode is finished
         done = True if self.window_counter == self.trading_window-1 else False
         
+        #Set next step's state
         if done==False:
             self.state = (self.features.iloc[self.window_counter+1,:], pos, abs(pos)-abs(pos_limit), self.trading_window-self.window_counter, self.stop_loss, self.take_profit)
         
-        ''' Reward function '''
-        #calculate returns up to today's close
+        
+        
+        #Calculate returns up to today's close
         close_open_returns = self.prices.iloc[-1]['Close'] / self.prices.iloc[:self.window_counter+1]['Open']
-        close_returns = [1+trade_log[i]*(close_open_returns[i]-1) for i in range(len(close_open_returns))]
-                
+        close_returns = [1+trade_log[i]*(close_open_returns[i]-1) for i in range(len(close_open_returns))]       
         total_return = np.prod(close_returns)
-                
+        
+        #Store returns up to each day's close
         self.daily_returns.append(total_return)
         #sharpe = total_return/np.std(self.daily_returns)
         
-        '''
-        #Next day's open vs today's open
-        if self.window_counter < self.trading_window-1:
-            open_open_return = self.prices.iloc[self.window_counter+1]['Open'] / self.prices.iloc[self.window_counter]['Open']
-        else:
-            open_open_return = self.prices.iloc[self.window_counter]['Close'] / self.prices.iloc[self.window_counter]['Open']
-        '''
+        
+        #Calculate next day's open over all previous days' open (for stop_loss & take_profit trigger)
         if self.window_counter < self.trading_window-1:
             open_open_return = self.prices.iloc[self.window_counter+1]['Open'] / self.prices.iloc[:self.window_counter+1]['Open']
         else:
             open_open_return = self.prices.iloc[self.window_counter]['Close'] / self.prices.iloc[:self.window_counter+1]['Open']
-        
-
+        #Calculate active return (marked at Open_t+1)
         if pos != 0:
             self.today_return = [1+trade_log[i]*(open_open_return[i]-1) for i in range(self.day_pos,self.window_counter+1)]
             self.active_return = np.prod(self.today_return)
         else:
             self.active_return = 1   
         
-        #average return of the following days
+        
+        #Calculate average return of the following days (for reward function)
         avg_return = (self.prices.iloc[self.window_counter:]['Close']).mean()/self.prices.iloc[self.window_counter]['Open']
         
+        #Reward function weights
         c_return = 1
         c_daily_return = 0.85
         c_hold = -0.02
@@ -142,7 +143,8 @@ class MarketEnv(gym.Env):
 
         reward = c_return*reward_ + c_daily_return* np.sign(avg_return-1) + c_hold*self.hold_time + c_trades*sum(np.absolute(trade_log)) + c_inactive*self.inactive_time
         
-        self.window_counter += self.trading_freq
+        
+        self.window_counter += self.trading_freq #Update window_counter
         
         return np.array(self.state[0].tolist()+[self.state[1], self.state[2]]), reward, done, {'trades': self.trade_log, 'actions': self.action_log, 'pos': self.pos_log, 'prices': self.prices, 'total_return': total_return, 'close_return': close_returns, 'close_open_returns': close_open_returns, 'daily_return': close_returns[-1], 'stop_loss_log': self.stop_loss_log}
         
